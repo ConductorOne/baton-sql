a83e9c167343d5e2ac62f527e67c1071ddf3d353
Justin Gallardo <justin.gallardo@conductorone.com>
2024-10-04 15:54:27 -0700

    Add support for creating a database connection using the DSN. Only MySQL is currently supported.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.6
 
 require (
 	github.com/conductorone/baton-sdk v0.2.31
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
---
+++ pkg/database/database.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"net/url"
+	"os"
+	"regexp"
+
+	"github.com/conductorone/baton-sql/pkg/database/mysql"
+)
+
+var DSNREnvRegex = regexp.MustCompile(`\$\{([A-Za-z0-9_]+)\}`)
+
+func updateDSNFromEnv(ctx context.Context, dsn string) (string, error) {
+	var err error
+
+	result := DSNREnvRegex.ReplaceAllStringFunc(dsn, func(match string) string {
+		varName := match[2 : len(match)-1]
+
+		value, exists := os.LookupEnv(varName)
+		if !exists {
+			err = errors.Join(err, fmt.Errorf("environment variable %s is not set", varName))
+			return match
+		}
+		return value
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
+}
+
+func Connect(ctx context.Context, dsn string) (*sql.DB, error) {
+	populatedDSN, err := updateDSNFromEnv(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	parsedDsn, err := url.Parse(populatedDSN)
+	if err != nil {
+		return nil, err
+	}
+
+	switch parsedDsn.Scheme {
+	case "mysql":
+		return mysql.Connect(ctx, populatedDSN)
+	default:
+		return nil, fmt.Errorf("unsupported database scheme: %s", parsedDsn.Scheme)
+	}
+}
