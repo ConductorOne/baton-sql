9243a079d9ee308b83fa8dc4eb07af6fea263259
Justin Gallardo <justin.gallardo@conductorone.com>
2024-10-11 13:18:53 -0700

    Test fixes

--- pkg/bcel/bcel_test.go
+++ pkg/bcel/bcel_test.go
@@ -17,6 +17,9 @@ func TestTemplateEnv_Evaluate(tt *testing.T) {
 		for _, op := range fn.Overloads {
 			for _, tc := range op.TestCases {
 				tt.Run(fmt.Sprintf("%s/%s", fn.Name, op.Operator), func(t *testing.T) {
+					if tc.Inputs == nil {
+						tc.Inputs = map[string]interface{}{}
+					}
 					env, err := NewEnv(ctx)
 					require.NoError(t, err)
 					out, err := env.Evaluate(ctx, tc.Expr, tc.Inputs)
