09a5e68c8f8c024ee0826a3dff9206cc310b3c0b
Geoff Greer <geoff@greer.fm>
2025-10-02 09:25:22 -0700

    Don't override default max conns or conn lifetime in DB drivers.

--- pkg/database/hdb/hdb.go
+++ pkg/database/hdb/hdb.go
@@ -3,15 +3,8 @@ package hdb
 import (
 	"context"
 	"database/sql"
-	"time"
 
 	_ "github.com/SAP/go-hdb/driver"
-)
-
-const (
-	MaxIdleConns    = 10
-	MaxOpenConns    = 10
-	MaxConnLifetime = 5 * time.Minute
 )
 
 func Connect(ctx context.Context, dsn string) (*sql.DB, error) {
@@ -19,10 +12,6 @@ func Connect(ctx context.Context, dsn string) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	db.SetMaxOpenConns(MaxOpenConns)
-	db.SetMaxIdleConns(MaxIdleConns)
-	db.SetConnMaxLifetime(MaxConnLifetime)
 
 	return db, nil
 }
